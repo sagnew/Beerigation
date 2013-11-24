b7a3b16485d65d56df0cf11ac28b88513765c2ed
yesdnil5 <yesdnil5@gmail.com>
2013-11-24 11:36:06 -0500

    scripts have new folders

--- arrayOfBeers.py
+++ arrayOfBeers.py
@@ -2,9 +2,8 @@ import csv
 
 def arrayOfBeers():
 	beer = []
-	with open('beer.csv', 'rb') as infile:
+	with open('dataScripts/beer.csv', 'rb') as infile:
 		reader = csv.reader(infile)
 		for row in reader:
 			beer.append(row[1])
-	print beer
 	return beer
