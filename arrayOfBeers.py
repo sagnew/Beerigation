1011c8381025c20e744b0102e069c0c69887ecb1
yesdnil5 <yesdnil5@gmail.com>
2013-11-24 10:18:05 -0500

    array of beers

---
+++ arrayOfBeers.py
@@ -0,0 +1,12 @@
+import csv
+
+def arrayOfBeers():
+	beer = []
+	with open('beer.csv', 'rb') as infile:
+		reader = csv.reader(infile)
+		for row in reader:
+			beer.append(row[1])
+	print beer
+	return beer
+
+arrayOfBeers()
